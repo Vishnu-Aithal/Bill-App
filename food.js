4d5d6420d688d98037ef39266e5c77b3f1ffb0cd
Vishnu-Aithal <90306959+Vishnu-Aithal@users.noreply.github.com>
2021-12-30 14:47:34 +0530

    feature - History with locale storage (#1)

--- food.js
+++ food.js
@@ -1,8 +1,22 @@
 const itemInputs = document.querySelectorAll(".item-input");
 const taxInputs = document.querySelectorAll(".tax-input");
 const btnAdd = document.querySelector("#btn-add");
-const itemList = document.querySelector(".items");
+const itemListDisplay = document.querySelector(".items");
 const output = document.querySelector(".output-value");
+const btnClear = document.querySelector("#btn-clear");
+const btnSaveClear = document.querySelector("#btn-save-clear");
+const btnClearHistory = document.querySelector("#btn-clear-history");
+const customerName = document.querySelector(".customer-name");
+const historyListDisplay = document.querySelector(".history");
+var currentList = []
+var previousBills = []
+
+window.onload = () => {
+    if (localStorage.getItem("previousBills")) {
+        previousBills = JSON.parse(localStorage.getItem("previousBills"))
+        populateHistroy(previousBills);
+    }
+}
 
 const itemInputsEntered = () => {
     if (itemInputs[0].value !== "" && Number(itemInputs[1].value) !== 0) return true;
@@ -12,6 +26,20 @@ const itemInputsEntered = () => {
 const resetItemInput = () => {
     itemInputs.forEach(input => input.value = "");
     btnAdd.disabled = true;
+}
+
+const updateMemory = function (name, price, method) {
+    itemBody = {
+        itemName: name,
+        itemPrice: price
+    }
+    if (method === "add") {
+        currentList.push(itemBody)
+    } else {
+        let indexToDelete = currentList.indexOf(itemBody);
+        currentList.splice(indexToDelete, 1)
+    }
+
 }
 const calculateTotal = () => {
     let finalAmount = 0
@@ -24,11 +52,14 @@ const calculateTotal = () => {
         let serviceTax = Number(taxInputs[1].value);
         finalAmount = sum * tax + serviceTax;
         output.innerText = finalAmount.toFixed(2);
-    } else output.innerText = "";
+        btnSaveClear.disabled = false;
+    } else {
+        output.innerText = "";
+        btnSaveClear.disabled = true
+    }
 }
-const addItem = function () {
-    let name = itemInputs[0].value;
-    let price = itemInputs[1].value;
+const addItem = function (name, price) {
+    updateMemory(name, price, "add")
     resetItemInput();
     let item = document.createElement('li')
     item.classList.add("list-item")
@@ -40,12 +71,82 @@ const addItem = function () {
     btnRemove.innerText = "🗑️"
     btnRemove.onclick = () => {
         item.remove();
+        updateMemory(name, price, "delete")
         calculateTotal();
     }
     item.append(btnRemove);
-    itemList.append(item);
+    itemListDisplay.append(item);
     calculateTotal();
     itemInputs[0].focus();
+}
+
+const populateHistroy = function (listOfPreviousBills) {
+    for (bill of listOfPreviousBills) {
+        let id = listOfPreviousBills.indexOf(bill)
+        let billItem = document.createElement("li");
+        billItem.classList.add("bill-item");
+        let billHeader = document.createElement("div");
+        billHeader.innerText = `Name: ${bill["customer"]} Date: ${bill["date"]}`
+        let billBody = document.createElement("div");
+        let listDisplayString = "";
+        for (listItem of bill["items"]) {
+            listDisplayString += `<li>${listItem.itemName} - ${listItem.itemPrice} ₹</li>`;
+        }
+        listDisplayString += `<li>Tax: ${bill.tax} %</li><li>S.Tax: ${bill.serviceTax} ₹</li><li>Total: ${bill.total}</li>`
+        billBody.innerHTML = `
+        <button class="btn btn-primary mt-1" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${id}" aria-expanded="false" aria-controls="collapseExample">Show</button>
+        <div class="collapse mt-2" id="collapse${id}">
+            <div class="card card-body">
+            <ul>
+            ${listDisplayString}
+            </ul>
+            </div>
+        </div>
+        `
+        billItem.append(billHeader);
+        billItem.append(billBody);
+
+
+        historyListDisplay.append(billItem);
+    }
+}
+
+
+const clearAll = function () {
+    currentList = [];
+    for (i = itemListDisplay.children.length - 1; i > 0; i--) {
+        itemListDisplay.children[i].remove()
+    }
+    for (input of document.querySelectorAll("input")) {
+        input.value = ""
+    }
+    output.innerText = "";
+    btnAdd.disabled = true;
+    btnSaveClear.disabled = true;
+}
+
+const saveAndClear = function () {
+
+    let bill = {
+        customer: customerName.value,
+        date: Date().slice(0, 24),
+        items: currentList,
+        tax: taxInputs[0].value,
+        serviceTax: taxInputs[1].value,
+        total: Number(output.innerText)
+    }
+
+    previousBills.push(bill)
+    localStorage.setItem("previousBills", JSON.stringify(previousBills))
+    clearAll()
+    historyListDisplay.innerHTML = ""
+    populateHistroy(previousBills)
+}
+
+const clearHistory = function () {
+    localStorage.removeItem("previousBills");
+    historyListDisplay.innerHTML = ""
+    previousBills = [];
 }
 
 itemInputs.forEach(input => {
@@ -64,13 +165,19 @@ itemInputs.forEach(input => {
     }
 })
 
-itemInputs[0].addEventListener("keyup", (e)=>{
-    if(e.key === "Enter") itemInputs[1].focus();
+customerName.addEventListener("keyup", (e) => {
+    if (e.key === "Enter" && e.target.value != "") itemInputs[0].focus();
 })
 
-itemInputs[1].addEventListener('keyup', (e)=>{
-    if(e.key === "Enter" && !btnAdd.disabled) addItem();
+itemInputs[0].addEventListener("keyup", (e) => {
+    if (e.key === "Enter" && e.target.value != "") itemInputs[1].focus();
 })
+
+itemInputs[1].addEventListener('keyup', (e) => {
+    if (e.key === "Enter" && !btnAdd.disabled) addItem(itemInputs[0].value, itemInputs[1].value);
+})
+
+
 
 taxInputs.forEach(input => {
     input.oninput = event => {
@@ -83,4 +190,7 @@ taxInputs.forEach(input => {
         }
     }
 })
-btnAdd.addEventListener("click", addItem)
+btnAdd.addEventListener("click", () => addItem(itemInputs[0].value, itemInputs[1].value))
+btnClear.addEventListener("click", clearAll)
+btnSaveClear.addEventListener("click", saveAndClear)
+btnClearHistory.addEventListener("click", clearHistory)
